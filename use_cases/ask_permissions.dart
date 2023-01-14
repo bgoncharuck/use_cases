d0064dca75877eae4c7e4b1c5adf2f87183a2aaa
Bohdan Honcharuk <dariydragon26@gmail.com>
2023-01-14 13:50:31 +0200

    Update ask_permissions.dart

--- use_cases/ask_permissions.dart
+++ use_cases/ask_permissions.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'i_use_case.dart';
 import '/services/analytics/analytics.dart';
@@ -9,15 +10,20 @@ class AskPermissions with IUseCase<bool, AskPermissionsParams> {
 
   @override
   Future<bool> execute({required AskPermissionsParams params}) async {
-    if (params.permissions.isEmpty) {
+    if (params.requiredPerms.isEmpty && params.optionalPerms.isEmpty) {
       return true;
     }
 
-    final Map<Permission, bool> status = {};
     final Map<String, String> analyticsStatus = {};
-    for (final permission in params.permissions) {
-      status[permission] = permissionIsGranted(await permission.request());
-      analyticsStatus[permission.toString()] = status[permission]! ? 'yes' : 'no';
+
+    final Map<Permission, bool> requiredStatus = {};
+    for (final permission in params.requiredPerms) {
+      requiredStatus[permission] = await requiredPermissionRequest(permission);
+      analyticsStatus[permission.toString()] = requiredStatus[permission]! ? 'yes' : 'no';
+    }
+
+    for (final permission in params.optionalPerms) {
+      analyticsStatus[permission.toString()] = await optionalPermissionRequest(permission) ? 'yes' : 'no';
     }
 
     Get.find<AnalyticsObserver>().event(
@@ -29,19 +35,65 @@ class AskPermissions with IUseCase<bool, AskPermissionsParams> {
       ),
     );
 
-    return status.values.every((granted) => granted);
-  }
-
-  bool permissionIsGranted(PermissionStatus status) {
-    return status == PermissionStatus.granted || status == PermissionStatus.limited;
+    if (requiredStatus.isEmpty) {
+      return true;
+    }
+    return requiredStatus.values.every((granted) => granted);
   }
 }
 
 class AskPermissionsParams {
   const AskPermissionsParams({
-    required this.permissions,
+    this.requiredPerms = const <Permission>[],
+    this.optionalPerms = const <Permission>[],
     required this.fromScreen,
   });
-  final List<Permission> permissions;
+  final List<Permission> requiredPerms;
+  final List<Permission> optionalPerms;
   final String fromScreen;
+}
+
+Future<bool> checkPermissions(List<Permission> permissions) async {
+  bool result = true;
+  for (final perm in permissions) {
+    if (permissionIsGranted(await perm.status) == false) {
+      result = false;
+    }
+  }
+  return result;
+}
+
+bool permissionIsGranted(PermissionStatus status) {
+  return status == PermissionStatus.granted || status == PermissionStatus.limited;
+}
+
+Future<bool> optionalPermissionRequest(Permission permission) async {
+  try {
+    final status = await permission.status;
+    if (permissionIsGranted(status)) {
+      return true;
+    } else if (status == PermissionStatus.denied) {
+      return permissionIsGranted(await permission.request());
+    }
+  } catch (e, t) {
+    Sentry.captureException(e, stackTrace: t);
+  }
+  return false;
+}
+
+Future<bool> requiredPermissionRequest(Permission permission) async {
+  try {
+    final status = await permission.status;
+    if (permissionIsGranted(status)) {
+      return true;
+    } else if (status == PermissionStatus.denied) {
+      return permissionIsGranted(await permission.request());
+    } else if (status == PermissionStatus.permanentlyDenied) {
+      await openAppSettings();
+      return false;
+    }
+  } catch (e, t) {
+    Sentry.captureException(e, stackTrace: t);
+  }
+  return false;
 }
