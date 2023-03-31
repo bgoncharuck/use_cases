3c8718767087ab8707bbf86f20b623b4e57b3be0
Bohdan Honcharuk <dariydragon26@gmail.com>
2023-03-31 16:53:18 +0300

    Update extensions.dart

--- utils/extensions.dart
+++ utils/extensions.dart
@@ -1,13 +1,14 @@
 extension SliceIterable<T> on Iterable<T> {
   Iterable<List<T>> slices(int length) sync* {
-    if (length < 1) throw RangeError.range(length, 1, null, 'length');
+    if (length < 1) yield [];
+    final iterator = this.iterator;
 
-    var iterator = this.iterator;
     while (iterator.moveNext()) {
-      var slice = [iterator.current];
+      final slice = [iterator.current];
       for (var i = 1; i < length && iterator.moveNext(); i++) {
         slice.add(iterator.current);
       }
+
       yield slice;
     }
   }
