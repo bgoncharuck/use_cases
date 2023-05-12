6876ed4c3129c4302cbefb95ab29529108e78982
Bohdan Honcharuk <dariydragon26@gmail.com>
2023-05-12 16:41:31 +0300

    Create screenshot.dart

---
+++ services/screenshot.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:isolate';
+import 'dart:ui' as ui;
+import 'package:flutter/rendering.dart';
+
+      final repaintContext = roulettePreviewKey.currentContext;
+      if (repaintContext == null) {
+        return false;
+      }
+
+      RenderRepaintBoundary boundary = repaintContext.findRenderObject() as RenderRepaintBoundary;
+      ui.Image image = await boundary.toImage(pixelRatio: 1.0);
+      ByteData? byteData = await (image.toByteData(format: ui.ImageByteFormat.png));
+      Uint8List rawFrame = byteData!.buffer.asUint8List();
+
+      final fileInterceptor = await File(pathToFrame).create();
+      await fileInterceptor.writeAsBytes(rawFrame);
