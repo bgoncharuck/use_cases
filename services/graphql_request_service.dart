db55788d48b8cbdb2d311e8e1c68d92f3a9b3185
Bohdan Honcharuk <dariydragon26@gmail.com>
2023-10-16 18:28:19 +0300

    Create graphql_request_service.dart

---
+++ services/graphql_request_service.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+late RawGraphQLService rawGraphQLService;
+
+class RawGraphQLService {
+  final String baseUrl;
+  final Map<String, String>? headers;
+
+  RawGraphQLService({required this.baseUrl, this.headers = const {}});
+
+  Future<dynamic> query({
+    required String query,
+    Map<String, dynamic>? variables,
+    Map<String, String>? headers,
+  }) async {
+    final response = await http.post(
+      Uri.parse(baseUrl),
+      headers: headers ?? this.headers,
+      body: json.encode({'query': query, 'variables': variables}),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to execute GraphQL query: ${response.body}');
+    }
+    return json.decode(response.body);
+  }
+
+  Future<dynamic> mutation({
+    required String mutation,
+    Map<String, dynamic>? variables,
+    Map<String, String>? headers,
+  }) async {
+    final response = await http.post(
+      Uri.parse(baseUrl),
+      headers: headers ?? this.headers,
+      body: json.encode({'mutation': mutation, 'variables': variables}),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to execute GraphQL mutation: ${response.body}');
+    }
+    return json.decode(response.body);
+  }
+}
