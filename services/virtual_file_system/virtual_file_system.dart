56e238092ad541d0d1278592e85c32a57cf67071
Bohdan Honcharuk <dariydragon26@gmail.com>
2023-11-09 17:22:56 +0200

    Create virtual_file_system.dart

---
+++ services/virtual_file_system/virtual_file_system.dart
@@ -0,0 +1,4 @@
+export 'file_system_interface.dart';
+export 'tree_file_system.dart';
+export 'virtual_file_system_creator.dart';
+export 'virtual_path.dart' show fileNodeToRealPath, fileNodeToVirtualPathPair, virtualPathToRealPathPair;
