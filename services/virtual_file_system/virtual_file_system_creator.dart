93b71f175ecf86c6b0a27e297d2afc43b7f5a5f1
Bohdan Honcharuk <dariydragon26@gmail.com>
2023-11-09 17:22:30 +0200

    Create virtual_file_system_creator.dart

---
+++ services/virtual_file_system/virtual_file_system_creator.dart
@@ -0,0 +1,59 @@
+import 'file_system_interface.dart';
+import 'tree_file_system.dart';
+import 'virtual_path.dart';
+
+FileSystem createVirtualSystem(List<String> realPaths, String rootNode) {
+  try {
+    final virtualPaths = prepareVirtualPaths(realPaths, rootNode);
+    final root = _buildTreeFileSystem(virtualPaths, rootNode);
+    return TreeFileSystem(root);
+  } catch (e, t) {
+    log.exception(e, t);
+    return TreeFileSystem(TreeFileNode(rootNode));
+  }
+}
+
+TreeFileNode _buildTreeFileSystem(List<String> virtualPaths, String rootNode) {
+  try {
+    fileNodeToVirtualPathPair.clear();
+
+    final root = TreeFileNode(rootNode);
+    for (final virtualPath in virtualPaths) {
+      final parts = virtualPath.split('/');
+      var current = root;
+      current = _traverseTree(parts, current);
+      final file = TreeFileNode(parts.last);
+      fileNodeToVirtualPathPair[file] = virtualPath;
+      current.children.add(file);
+    }
+    return root;
+  } catch (e, t) {
+    log.exception(e, t);
+    return TreeFileNode(rootNode);
+  }
+}
+
+TreeFileNode _traverseTree(List<String> parts, TreeFileNode initial) {
+  var current = initial;
+  try {
+    final len = parts.length - 1;
+    for (var i = 0; i < len; i++) {
+      final name = parts[i];
+      final child = current.findChild(name);
+      if (child != null) {
+        current = child as TreeFileNode;
+      } else {
+        current = _addDirectory(current, name);
+      }
+    }
+  } catch (e, t) {
+    log.exception(e, t);
+  }
+  return current;
+}
+
+TreeFileNode _addDirectory(TreeFileNode current, String name) {
+  final directory = TreeFileNode(name);
+  current.children.add(directory);
+  return directory;
+}
