9abe6756e00baed48050f73e66584dd841b6d038
Bohdan Honcharuk <dariydragon26@gmail.com>
2024-02-16 16:31:34 +0200

    Update idfa.dart

--- services/idfa.dart
+++ services/idfa.dart
@@ -1,11 +1,31 @@
 import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 
 Future<String> getIDFAId() async {
-  final status = await AppTrackingTransparency.requestTrackingAuthorization();
+  try {
+    final status = await AppTrackingTransparency.requestTrackingAuthorization();
 
-  if (status == TrackingStatus.authorized) {
-    return await AppTrackingTransparency.getAdvertisingIdentifier();
-  } else {
-    return 'TrackingStatus ${status.name}';
+    if (status == TrackingStatus.authorized) {
+      return await AppTrackingTransparency.getAdvertisingIdentifier();
+    } else {
+      return 'TrackingStatus ${status.name}';
+    }
+  } catch (e, t) {
+    await Sentry.captureException(e, stackTrace: t);
+    return 'TrackingStatus restricted';
+  }
+}
+
+int getStatusInteger(String status) {
+  switch (status) {
+    case 'TrackingStatus notDetermined':
+      return 0;
+    case 'TrackingStatus restricted':
+      return 1;
+    case 'TrackingStatus denied':
+      return 2;
+    case 'TrackingStatus authorized':
+      return 3;
+    default:
+      return -1;
   }
 }
